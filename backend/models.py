911c0922b90ce8396d2bf7c9fa969c15f124393e
Gabriel Couto <gabrielcouto26@gmail.com>
2023-10-12 23:55:31 -0300

    implementa model UserType

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class UserType(models.Model):
+    USER_TYPES = [
+        (1, 'Student'),
+        (2, 'Teacher'),
+        (3, 'Coordinator'),
+    ]
+
+    type = models.IntegerField(
+        choices=USER_TYPES,
+        default=1
+    )
+
+    class Meta:
+        db_table = 'user_types'
