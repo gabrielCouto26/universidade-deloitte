f6f23919f17b91742b23bccf6f375a4aec984504
Gabriel Couto <gabrielcouto26@gmail.com>
2023-10-13 21:50:51 -0300

    implementa model User

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    class UserType(models.IntegerChoices):
+        STUDENT = 1
+        TEACHER = 2
+        COORDINATOR = 3
+
+    user_type = models.IntegerField(
+        choices=UserType.choices,
+        default=UserType.STUDENT)
+
+    email = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    birthday = models.DateField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
