8bbd5ac5e1a8f4396a7a0a8803ef2c67e0ca7caa
Gabriel Couto <gabrielcouto26@gmail.com>
2023-10-18 22:48:54 -0300

    corrige cadastro de DISCIPLINE

--- backend/views/discipline.py
+++ backend/views/discipline.py
@@ -1,14 +1,40 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from backend.models.discipline import Discipline
+from backend.models.teacher import Teacher
+from backend.models.student import Student
 from backend.serializers.discipline_serializer import DisciplineSerializer
 from backend.serializers.student_serializer import StudentSerializer
 from backend.permissions import IsCoordinator, IsTeacher, CanList
 
 
 class DisciplineList(generics.ListCreateAPIView):
-    permission_classes = [IsCoordinator | IsTeacher, CanList]
+    permission_classes = [IsCoordinator | IsTeacher]
     queryset = Discipline.objects.all()
     serializer_class = DisciplineSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+
+        teacher_id = data.get('teacher')
+        student_ids = data.get('students', [])
+
+        teacher = Teacher.objects.get(id=teacher_id)
+
+        students = []
+        for student_id in student_ids:
+            student = Student.objects.get(id=student_id)
+            students.append(student)
+
+        discipline = Discipline.objects.create(
+            teacher=teacher,
+            name=data['name'],
+            workload=data['workload'])
+
+        discipline.students.set(students)
+
+        serializer = DisciplineSerializer(discipline)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class DisciplineDetail(generics.RetrieveUpdateDestroyAPIView):
