734d2c777a38c7441bba1201eaac1c83793e373b
Gabriel Couto <gabrielcouto26@gmail.com>
2023-10-18 23:00:27 -0300

    atualiza retorno de DISCIPLINES

--- backend/serializers/discipline_serializer.py
+++ backend/serializers/discipline_serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from backend.models.discipline import Discipline
+from backend.serializers.student_serializer import StudentSerializer
 
 
 class DisciplineSerializer(serializers.ModelSerializer):
@@ -11,7 +12,18 @@ class DisciplineSerializer(serializers.ModelSerializer):
         return obj.teacher.user.name if obj.teacher and obj.teacher.user else None
 
     def get_students(self, obj):
-        return obj.students.count()
+        """
+        Retorna lista de STUDENTS se a requisição for de detalhes.
+        Retorna contagem de STUDENTS se a requisição for de listagem.
+        """
+        if ("request" in self.context and
+                self.context["request"].resolver_match.url_name == "discipline"):
+
+            students = obj.students.all()
+            student_serializer = StudentSerializer(students, many=True)
+            return student_serializer.data
+        else:
+            return obj.students.count()
 
     class Meta:
         model = Discipline
